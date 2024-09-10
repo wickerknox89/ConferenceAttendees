6f2e727796b5d6b186f9b136006b82455fd1ca67
wickerknox <wickerknox89@gmail.com>
2024-09-10 12:44:33 -0700

    Adding classes in MVC project to interact with API

---
+++ ConferenceAttendees.MVC/Services/Base/Client.cs
@@ -0,0 +1,15 @@
+﻿namespace ConferenceAttendees.MVC.Services.Base
+{
+    public partial class Client : IClient
+    {
+        public HttpClient HttpClient
+        {
+            get
+            {
+                return _httpClient;
+            }
+        }
+    }
+}
+
+
---
+++ ConferenceAttendees.MVC/Services/Base/IClient.cs
@@ -0,0 +1,9 @@
+﻿namespace ConferenceAttendees.MVC.Services.Base
+{
+    public partial interface IClient
+    {
+        public HttpClient HttpClient { get; }
+    }
+}
+
+
