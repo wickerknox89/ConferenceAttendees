23b642777695557738ca2e8a80e4c5cb82ecdd3f
wickerknox <wickerknox89@gmail.com>
2024-09-10 11:21:41 -0700

    API : Adding Base classes and Configuration classes for seeding data

---
+++ ConferenceAttendees.API/Data/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConferenceAttendees.API.Data
+{
+    public abstract class BaseEntity
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public Guid Id { get; set; }
+    }
+}
+
+
