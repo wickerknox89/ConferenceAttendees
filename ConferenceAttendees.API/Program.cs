8d6c1581b380e019feb4ca274e1945f1e0cde9e7
wickerknox <wickerknox89@gmail.com>
2024-09-10 19:11:45 -0700

    Docker Compose : Add SQL Server container to Docker Compose with a new password, Add migration in Program.cs

--- ConferenceAttendees.API/Program.cs
+++ ConferenceAttendees.API/Program.cs
@@ -21,6 +21,10 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    using var scope=app.Services.CreateScope();
+    var db=scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
 }
 
 app.UseHttpsRedirection();
