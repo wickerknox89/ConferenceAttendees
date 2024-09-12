8a58842f2c57a56d7277954f428c86a9cec5cd70
wickerknox <wickerknox89@gmail.com>
2024-09-11 19:05:19 -0700

    Testing Docker Compose and Reverse Proxy

--- ConferenceAttendees.API/Program.cs
+++ ConferenceAttendees.API/Program.cs
@@ -1,4 +1,5 @@
 using ConferenceAttendees.API.Data;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -28,6 +29,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
 
 app.UseAuthorization();
 
