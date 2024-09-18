402bc47b88e3d520f2878ddc97c8d63fa173004e
wickerknox <wickerknox89@gmail.com>
2024-09-17 23:31:30 -0700

    Specify Environment Variables in Containers

--- ConferenceAttendees.API/Controllers/HomeController.cs
+++ ConferenceAttendees.API/Controllers/HomeController.cs
@@ -21,30 +21,6 @@ namespace ConferenceAttendees.API.Controllers
         {
             return _configuration["EnvironmentValue"];
         }
-
-        // GET api/<HomeController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<HomeController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<HomeController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<HomeController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
+
